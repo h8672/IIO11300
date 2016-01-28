cd75c171342eebfca71eb7f6baea276bb4a07119
Juha-Matti Kokkonen <juhamk91@gmail.com>
2016-01-28 09:32:09 +0200

    Tehtävä 2 toiminnassa.

---
+++ IIO11300Vktehtavat/Tehtava2/BLLotto.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava2
+{
+    class BLLotto
+    {
+        #region Attribuutit
+        private int game;
+        static Random rnd = new Random();
+        #endregion
+        #region Ominaisuudet
+        public int Game {
+            get { return game; }
+            set { game = value; }
+        }
+        #endregion
+        #region Rakentajat
+        #endregion
+        #region Funktiot
+        static public string GameName(int ID) {
+            if (ID == 0) return "Suomi";
+            else if (ID == 1) return "VikingLotto";
+            else if (ID == 2) return "Eurojackpot";
+            else return "";
+        }
+        static public int[] DrawNumber(int ID)
+        {
+            int[] luvut;
+            if (ID == 0) {
+                luvut = new int[7];
+                for (int i = 0; i < 7; i++)
+                {
+                    luvut[i] = (int)(39 * rnd.NextDouble());
+                }
+                return luvut;
+            }
+            else if (ID == 1)
+            {
+                luvut = new int[6];
+                for (int i = 0; i < 6; i++)
+                {
+                    luvut[i] = (int)(48 * rnd.NextDouble());
+                }
+                return luvut;
+            }
+            else if (ID == 2)
+            {
+                luvut = new int[7];
+                //Päänumerot
+                for (int i = 0; i < 5; i++)
+                {
+                    luvut[i] = (int)(49 * rnd.NextDouble() + 1);
+                }
+                //Tähtinumerot
+                for (int i = 5; i < 7; i++)
+                {
+                    luvut[i] = (int)(7 * rnd.NextDouble() + 1);
+                }
+                return luvut;
+            }
+            else return null;
+        }
+        #endregion
+    }
+
+}
---
+++ IIO11300Vktehtavat/Tehtava2/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtava2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            comboBox.Text = BLLotto.GameName(comboBox.SelectedIndex);
+            comboBox.Items.Add(BLLotto.GameName(0));
+            comboBox.Items.Add(BLLotto.GameName(1));
+            comboBox.Items.Add(BLLotto.GameName(2));
+            DrawTextBox.Text = "1";
+            comboBox.SelectedIndex = 0;
+            DrawsTextBox.Text = "";
+        }
+
+        private void Drawbtn_Click(object sender, RoutedEventArgs e) {
+            int draws = Convert.ToInt32(DrawTextBox.Text);
+            for(int i = 0; i < draws; i++) {
+                int[] luvut = BLLotto.DrawNumber(comboBox.SelectedIndex);
+                DrawsTextBox.Text = DrawsTextBox.Text + BLLotto.GameName(comboBox.SelectedIndex) + ":";
+                int test = 0;
+                foreach (int luku in luvut)
+                {
+                    if (DrawsTextBox.Text == "" || test == 0) DrawsTextBox.Text = DrawsTextBox.Text + " " + luku.ToString();
+                    else DrawsTextBox.Text = DrawsTextBox.Text + ", " + luku.ToString();
+                    test++;
+                }
+                DrawsTextBox.Text = DrawsTextBox.Text + ".\n";
+            }
+        }
+
+        private void Clearbtn_Click(object sender, RoutedEventArgs e) {
+            DrawTextBox.Text = "1";
+            DrawsTextBox.Text = "";
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            comboBox.Text = BLLotto.GameName(comboBox.SelectedIndex);
+        }
+
+        private void DrawTextBlock_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            DrawTextBox.Text = DrawTextBox.Text + sender.ToString();
+        }
+    }
+}
