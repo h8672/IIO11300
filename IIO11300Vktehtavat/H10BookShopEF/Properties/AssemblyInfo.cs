f97af5c78273f87ff2a6f7a5aa4dd93c01cd4987
Juha-Matti Kokkonen <juhamk91@gmail.com>
2016-04-07 08:45:36 +0300

    Making setup.exe

--- IIO11300Vktehtavat/H10BookShopEF/Properties/AssemblyInfo.cs
+++ IIO11300Vktehtavat/H10BookShopEF/Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("H10BookShopEF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BookShop")]
+[assembly: AssemblyDescription("Bookshop application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("H10BookShopEF")]
+[assembly: AssemblyCompany("student corpetetition")]
+[assembly: AssemblyProduct("IIO11300 products")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("J-M edition")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
